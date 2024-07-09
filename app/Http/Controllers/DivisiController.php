d33389c59528ec45a52bf068798086c81a6171fd
SyahidanNJ <hidanyomaen@gmail.com>
2024-07-09 08:37:27 +0700

    add: read divisi

--- app/Http/Controllers/DivisiController.php
+++ app/Http/Controllers/DivisiController.php
@@ -3,11 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\SQL\AdminDivisiSQL;
 
 class DivisiController extends Controller
 {
+    protected $DataDivisi;
+    public function __construct(AdminDivisiSQL $AdminDivisiSQL)
+    {
+        $this->DataDivisi = $AdminDivisiSQL;
+    }
     public function index()
     {
         return view('admin.divisi');
+    }
+    public function read(){
+        $data = $this->DataDivisi->getDivisiData();
+
+        return datatables()->of($data)
+            ->addIndexColumn()
+            ->make(true);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,8 @@ Route::get('/admin/data-user-pasien', [UserController::class, 'readPasien'])->na
 Route::get('/admin/data-user-dokter', [UserController::class, 'readDokter'])->name('admin-datauser-dokter');
 Route::get('/admin/data-user-admin', [UserController::class, 'readAdmin'])->name('admin-datauser-admin');
 
+Route::get('/admin/data-divisi', [DivisiController::class, 'read'])->name('admin-datadivisi');
+
 //Dokter
 Route::get('/dokter/dashboard', [DashboardController::class, 'indexDokter'])->name('dokter-dashboard');
 
