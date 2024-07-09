167b27a37c03a259ab2c5665c7e7cb51b20f945a
Muhamad Farrel Rizqullah <92018210+muhamadfarrelrizqullah@users.noreply.github.com>
2024-07-09 16:23:36 +0700

    fix: bahasa indo

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -6,11 +6,19 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class LoginRequest extends FormRequest
 {
+    /**
+     * Determine if the user is authorized to make this request.
+     */
     public function authorize()
     {
-        return true; // Set ke true jika ingin memungkinkan request untuk diproses
+        return true;
     }
 
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
     public function rules()
     {
         return [
