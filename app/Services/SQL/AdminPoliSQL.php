459023e472b68f8f091dcc8c133daca0ca3d4c41
Muhamad Farrel Rizqullah <92018210+muhamadfarrelrizqullah@users.noreply.github.com>
2024-07-11 10:38:07 +0700

    add: jumlah user kolom

--- app/Services/SQL/AdminPoliSQL.php
+++ app/Services/SQL/AdminPoliSQL.php
@@ -3,14 +3,19 @@
 namespace App\Services\SQL;
 
 use App\Models\Poli;
+use Illuminate\Support\Facades\DB;
 
 class AdminPoliSQL
 {
     public function getPoliData()
     {
         return Poli::select([
-            'id',
-            'nama',
-        ]);
+            'polis.id',
+            'polis.nama',
+            DB::raw('COUNT(pivot_polis_users.id) as user_count')
+        ])
+        ->leftJoin('pivot_polis_users', 'polis.id', '=', 'pivot_polis_users.id_poli')
+        ->groupBy('polis.id', 'polis.nama')
+        ->get();
     }
 }
