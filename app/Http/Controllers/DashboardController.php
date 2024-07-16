6c8fe57b5ae8db8db25ce0c5dc08153c5a380db8
Muhamad Farrel Rizqullah <92018210+muhamadfarrelrizqullah@users.noreply.github.com>
2024-07-16 10:17:38 +0700

    feat: dashboard pasien

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -37,20 +37,18 @@ class DashboardController extends Controller
     public function indexPasien()
     {
         $pengajuan = Pengajuan::where('id_pasien', auth()->user()->id)->get();
-        // dd($pengajuan);
         $user = auth()->user();
         return view('pasien.dashboard', compact('pengajuan', 'user'));
     }
 
     public function dasboardPasien(Request $request)
     {
-        $userId = auth()->user()->id; // Get the currently logged-in user's ID
+        $userId = auth()->user()->id;
 
         $pengajuan = Pengajuan::where('id_pasien', $userId)
             ->join('users', 'users.id', '=', 'pengajuans.id_pasien')
             ->select('pengajuans.*', 'users.nama as nama_pasien')
-            ->get(); // Filter submissions by user ID
-        // dd($pengajuan);
+            ->get();
 
         return datatables()->of($pengajuan)
             ->addIndexColumn()
