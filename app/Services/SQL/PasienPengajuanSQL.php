a65c4c7f9e2385b8693fffcf40b2708bd9d87fd3
Muhamad Farrel Rizqullah <92018210+muhamadfarrelrizqullah@users.noreply.github.com>
2024-07-24 10:33:29 +0700

    fix: detail qrcode modal pasien

--- app/Services/SQL/PasienPengajuanSQL.php
+++ app/Services/SQL/PasienPengajuanSQL.php
@@ -13,7 +13,7 @@ class PasienPengajuanSQL
 
         return Pengajuan::join('users as pasien', 'pengajuans.id_pasien', '=', 'pasien.id')
             ->leftJoin('users as dokter', 'pengajuans.id_dokter', '=', 'dokter.id')
-            ->join('polis', 'pengajuans.id_poli', '=', 'polis.id')
+            ->leftJoin('polis', 'pengajuans.id_poli', '=', 'polis.id')
             ->select([
                 'pengajuans.id',
                 'pengajuans.keluhan',
